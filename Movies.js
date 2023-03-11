0a83c55ecfb94fc9e27694771309536ea555b4d8
Emeka Mgbemere <41328704+EmekaMgbemere@users.noreply.github.com>
2023-03-11 08:57:35 +0100

    Add files via upload

---
+++ Movies.js
@@ -0,0 +1,50 @@
+export const Movies = [
+    {
+        "id": 1,
+        "name": "Spiderman",
+        "pg":"13",
+        "duration": "110",
+        "genre":"Action",
+        "img":require("../../components/images/spiderman.png")
+    },
+    {
+        "id": 2,
+        "name": "Batman",
+        "pg":"15",
+        "duration": "120",
+        "genre":"Action",
+        "img":require("../../components/images/batman.png")
+    },
+    {
+        "id": 3,
+        "name": "Superman",
+        "pg":"14",
+        "duration": "150",
+        "genre":"Action",
+        "img":require("../../components/images/superman.png")
+    },
+    {
+        "id": 4,
+        "name": "The Witcher",
+        "pg":"15",
+        "duration": "130",
+        "genre":"Action",
+        "img": require("../../components/images/thewitcher.png")
+    },
+    {
+        "id": 5,
+        "name": "Black Widow",
+        "pg":"15",
+        "duration": "130",
+        "genre":"Action",
+        "img":require("../../components/images/blackwidow.png")
+    },
+    {
+        "id": 6,
+        "name": "Aquaman",
+        "pg":"15",
+        "duration": "80",
+        "genre":"Action",
+        "img": require("../../components/images/aquaman.png")
+    }
+]
